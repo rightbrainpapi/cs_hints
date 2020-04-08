d1f17b70171d9149f01dd7b3627f71335a5e54e3
rightbrainpapi <dahuemaen@gmail.com>
2020-04-07 20:27:00 -0400

    adding the edit icon

--- src/cs_hints/CsHInts.js
+++ src/cs_hints/CsHInts.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { FaEdit } from "react-icons/fa";
 
 const CsHints = () => {
 	return (
@@ -10,15 +11,17 @@ const CsHints = () => {
 						src="https://cdn.glitch.com/b75055dd-03c2-47e5-9f5d-7923ac439cc1%2Fdarnell.png?v=1584322716006"
 						alt="userPic"
 					/>
-
+					<div className="userEdit">
+						<FaEdit className="userEditIco" />
+					</div>
 					<h1 className="userName">Darnell</h1>
 				</div>
-				<div className="NavSec">
-					<button>CS Hints</button>
-					<button>Profile</button>
-					<button>Resources</button>
-					<button>News</button>
-					<button>Logout</button>
+				<div className="navSec">
+					<button className="navBtn">CS Hints</button>
+					<button className="navBtn">Profile</button>
+					<button className="navBtn">Resources</button>
+					<button className="navBtn">News</button>
+					<button className="navBtn">Logout</button>
 				</div>
 			</div>
 			<div className="displaySec"></div>
