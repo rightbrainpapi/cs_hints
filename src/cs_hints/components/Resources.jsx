3e9f16da4bbac7423716d15fb07d6659c91a8ed6
rightbrainpapi <dahuemaen@gmail.com>
2020-04-09 11:55:27 -0400

    changing where the resources css lives

--- src/cs_hints/components/Resources.jsx
+++ src/cs_hints/components/Resources.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-// import "./button.css";
+import "./styles/resources.css";
 
 const Resources = (props) => {
 	return <div className="resourcesPage">This is the Resource Page</div>;
